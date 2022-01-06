3b54ab6cdb231973d46fc8eb1fcda43eab33b5c0
partainq <partainq@gmail.com>
2022-01-06 09:50:50 -0500

    included flipped coin | be sure to flip the token_dev comments/variables :)

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import slack
 import os
+import random
 from pathlib import Path
 from dotenv import load_dotenv
 from flask import Flask
@@ -9,9 +10,12 @@ env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 app = Flask(__name__)
-slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'],'/slack/events', app)
+slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET_DEV'],'/slack/events', app)
+#slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'],'/slack/events', app)
 
-client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+client = slack.WebClient(token=os.environ['SLACK_TOKEN_DEV'])
+#client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+
 BOT_ID = client.api_call('auth.test')['user_id']
 
 @slack_event_adapter.on('message')
@@ -32,10 +36,19 @@ def message(payload):
         if numbers[1] == "+":
             client.chat_postMessage(channel=channel_id, text=int(numbers[0]) + int(numbers[2]))
 
-    if user_id != BOT_ID and flag == True:
+    elif "flip a coin" in text.lower():
+        channel_id = event.get("channel")
+        rand_int = random.randint(0, 1)
+        if rand_int == 0:
+            results = "Heads"
+        else:
+            results = "Tails"
+        message = "The result is "+results
+
+        client.chat_postMessage(channel=channel_id, text=message)
+
+    elif user_id != BOT_ID and flag == True:
         client.chat_postMessage(channel=channel_id, text=text)
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
