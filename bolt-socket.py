d8a830394453284b6d6a22ee7856916e62cdfb58
partainq <partainq@gmail.com>
2022-01-10 14:53:22 -0500

    bolt socket greeting, help, etc

--- bolt-socket.py
+++ bolt-socket.py
@@ -1,19 +1,15 @@
-#trying something new..
 import logging
 import os
 import re
 import random
 import requests, json
 
-import formats.weather
+import formats.weather, formats.help
 
 from dotenv import load_dotenv
 import pyjokes
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
-
-# gooda = xapp-1-A02SQ341ZCL-2926074915076-9b5c250da6ad7ec38d1ee832aca98e18acde5f0eebd3864d3abe8b540aa35854
-
 
 load_dotenv()
 
@@ -24,12 +20,39 @@ WEATHER_KEY = os.environ["WEATHER_KEY"]
 app = App(token=SLACK_TOKEN_DEV, name="ben")
 logger = logging.getLogger(__name__)
 
+@app.message(re.compile("hi|hey|hello"))
+def greeting(message, say):
+    channel_type = message["channel_type"]
+    if channel_type != "im": #im = direct channel
+        return
+
+    dm_channel = message["channel"]
+    user_id = message["user"]
+
+    name = app.client.users_info(user=user_id)["user"]["name"]
+    greeting = random.choice(["Hi there, ", "Hey, ", "Hello, ", "Great to hear from you, ", "Hi, "]) + name + "!"
+
+    say(text=greeting, channel=dm_channel)
+
+@app.message(re.compile("whats up|what up|what's up|sup"))
+def greeting(message, say):
+    channel_type = message["channel_type"]
+    if channel_type != "im": #im = direct channel
+        return
+
+    dm_channel = message["channel"]
+    user_id = message["user"]
+
+    message = random.choice(["Not much. Life is pretty quiet as a chatbot.", "Some movie about an old guy, balloons, and a flying house", "What can I help you with today?", "Great to hear from you."])
+
+    say(text=message, channel=dm_channel)
+
 
 @app.message(re.compile("joke"))  # type: ignore
 def random_joke(message, say):
     channel_type = message["channel_type"]
-    #if channel_type != "im": #im = direct channel
-    #    return
+    if channel_type != "im": #im = direct channel
+        return
 
     dm_channel = message["channel"]
     user_id = message["user"]
@@ -37,11 +60,24 @@ def random_joke(message, say):
     joke = pyjokes.get_joke()
     say(text=joke, channel=dm_channel)
 
+@app.message(re.compile("what are you|who are you|help|can you do|purpose|capabilities"))
+def help_message(message, say):
+    channel_type = message["channel_type"]
+    if channel_type != "im": #im = direct channel
+        return
+
+    dm_channel = message["channel"]
+    user_id = message["user"]
+
+    blocks = formats.help.getFormat()
+
+    say(blocks=blocks, text="hi", channel=dm_channel)
+
 @app.message(re.compile("flip a coin" or "coin flip"))
 def coin_flip(message, say):
     channel_type = message["channel_type"]
-    #if channel_type != "im":
-    #    return
+    if channel_type != "im":
+        return
     
     dm_channel = message["channel"]
     rand_int = random.randint(0, 1)
@@ -55,8 +91,8 @@ def coin_flip(message, say):
 @app.message(re.compile("weather"))
 def weather(message, say):
     channel_type = message["channel_type"]
-    #if channel_type != "im":
-    #    return
+    if channel_type != "im":
+        return
     
     dm_channel = message["channel"]
     lat = "40"
@@ -72,7 +108,7 @@ def weather(message, say):
     print(request)
     blocks = formats.weather.getFormat(temp, icon, description)
 
-    say(blocks=blocks, text="hi", channel=dm_channel)
+    say(blocks=blocks, text="weather", channel=dm_channel)
 
 def main():
     handler = SocketModeHandler(app, SOCKET_TOKEN_DEV)
---
+++ formats/help.py
@@ -0,0 +1,31 @@
+def getFormat():
+    return [
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "Hey there 👋 I'm Ben. I'm here to help you and answer Taylor related questions. Ask for the weather, a joke, or just say hey sometime! I go above and beyond to be extra nice. I also have several other helpful capabilities, such as:"
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "1️⃣ *Setup a study table* for you. Text me and I can anonymously notify your proffessor the need for a study session. Simply text in a DM, 'study table', and we can get the process started."
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "2️⃣ *View class rosters*, and see who is in your class! Helpful for teachers and students"
+			}
+		},
+				{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "3️⃣ *View next semester classes*, all you have to do is ask!"
+			}
+		}
+	]
