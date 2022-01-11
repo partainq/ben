4e4a486b1072529ec951090252aa7ca3470c9e77
partainq <partainq@gmail.com>
2022-01-11 14:18:35 -0500

    new functionality

--- bolt-socket.py
+++ bolt-socket.py
@@ -20,10 +20,11 @@ WEATHER_KEY = os.environ["WEATHER_KEY"]
 app = App(token=SLACK_TOKEN_DEV, name="ben")
 logger = logging.getLogger(__name__)
 
-@app.message(re.compile("hi|hey|hello"))
+
+@app.message(re.compile("hi|hey|hello", re.I))
 def greeting(message, say):
     channel_type = message["channel_type"]
-    if channel_type != "im": #im = direct channel
+    if channel_type != "im": 
         return
 
     dm_channel = message["channel"]
@@ -34,8 +35,21 @@ def greeting(message, say):
 
     say(text=greeting, channel=dm_channel)
 
-@app.message(re.compile("whats up|what up|what's up|sup"))
-def greeting(message, say):
+@app.message(re.compile("how are you|how is your day|how was your day|how are ya", re.I))
+def question_greeting(message, say):
+    channel_type = message["channel_type"]
+    if channel_type != "im": #im = direct channel
+        return
+
+    dm_channel = message["channel"]
+    user_id = message["user"]
+
+    message = random.choice(["Having another day to help people is a blessing.", "Looks like my server is still up, so it is a good day!", "It's a good day to have a good day!", "Wonderful!"])
+
+    say(text=message, channel=dm_channel)
+
+@app.message(re.compile("whats up|what up|what's up|sup", re.I))
+def casual_greeting(message, say):
     channel_type = message["channel_type"]
     if channel_type != "im": #im = direct channel
         return
@@ -47,8 +61,7 @@ def greeting(message, say):
 
     say(text=message, channel=dm_channel)
 
-
-@app.message(re.compile("joke"))  # type: ignore
+@app.message(re.compile("joke", re.I))  # type: ignore
 def random_joke(message, say):
     channel_type = message["channel_type"]
     if channel_type != "im": #im = direct channel
@@ -60,7 +73,7 @@ def random_joke(message, say):
     joke = pyjokes.get_joke()
     say(text=joke, channel=dm_channel)
 
-@app.message(re.compile("what are you|who are you|help|can you do|purpose|capabilities"))
+@app.message(re.compile("what are you|who are you|do you do|help|can you do|purpose|capabilities", re.I))
 def help_message(message, say):
     channel_type = message["channel_type"]
     if channel_type != "im": #im = direct channel
@@ -73,7 +86,7 @@ def help_message(message, say):
 
     say(blocks=blocks, text="hi", channel=dm_channel)
 
-@app.message(re.compile("flip a coin" or "coin flip"))
+@app.message(re.compile("flip a coin" or "coin flip", re.I))
 def coin_flip(message, say):
     channel_type = message["channel_type"]
     if channel_type != "im":
@@ -88,7 +101,7 @@ def coin_flip(message, say):
 
     say(text=results, channel=dm_channel)
 
-@app.message(re.compile("weather"))
+@app.message(re.compile("weather", re.I))
 def weather(message, say):
     channel_type = message["channel_type"]
     if channel_type != "im":
@@ -110,6 +123,35 @@ def weather(message, say):
 
     say(blocks=blocks, text="weather", channel=dm_channel)
 
+@app.message(re.compile("next semester|next term|classes next|next classes|next year", re.I))
+def weather(message, say):
+    channel_type = message["channel_type"]
+    if channel_type != "im":
+        return
+
+    url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=imperial" % (lat, lon, WEATHER_KEY)
+    request = json.loads(requests.get(url).text)["current"]
+
+    icon = request["weather"][0]['icon']
+    temp = request["temp"]
+    description= request["weather"][0]["main"]
+    dm_channel = message["channel"]
+
+    print(request)
+    blocks = formats.weather.getFormat(temp, icon, description)
+
+    say(blocks=blocks, text="weather", channel=dm_channel)
+
+@app.event("team_join")
+def ask_for_introduction(event, say):
+    welcome_channel_id = "C12345"
+    user_id = event["user"]
+    #dm_channel = message["channel"]
+    text = f"Welcome to the team, <@{user_id}>! 🎉 You can introduce yourself in this channel."
+    #say(text=text, channel=dm_channel)
+    print(event)
+
+
 def main():
     handler = SocketModeHandler(app, SOCKET_TOKEN_DEV)
     handler.start()
@@ -117,3 +159,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+    #https://cse.taylor.edu/information/faq/
